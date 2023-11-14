5168b5b227f669e20748ae6bcad472c988104666
alakt13 <114614671+alakt13@users.noreply.github.com>
2023-11-14 03:41:15 -0500

    smooth function added!

---
+++ SourceCode/two_dim_smooth_function.m
@@ -0,0 +1,9 @@
+% dai = axial displacement
+% dli = lateral displacement 
+% dai_prev = the previous axial displacement
+% dli_prev = the previous lateral displacemen
+
+
+function smoothness_2d = two_dim_smooth_function(dai, dli, dai_prev, dli_prev)
+    smoothness_2d = (dai - dai_prev)^2 + (dli - dli_prev)^2;
+end
