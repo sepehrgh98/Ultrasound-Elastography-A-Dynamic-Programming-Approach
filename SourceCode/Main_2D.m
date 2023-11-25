ac490e18f19985c0b92ac5398dd633450d6e366b
sepehr ghamari <sepehrghamri@gmail.com>
2023-11-25 17:20:49 -0500

    Main_2D has debugged!

--- SourceCode/Main_2D.m
+++ SourceCode/Main_2D.m
@@ -1,25 +1,26 @@
 clc;
 clear;
+close all;
 
 % Loading Data
-OriginalImg = [1 2 3 4 5;6 7 8 9 10;11 12 13 14 15; 16 17 18 19 20];
-CompressedImg = [2 3 4 5 6;7 8 9 10 11;6 7 9 10 15; 4 6 12 15 16];
+% OriginalImg = [1 2 3 4 5;6 7 8 9 10;11 12 13 14 15; 16 17 18 19 20];
+% CompressedImg = [2 3 4 5 6;7 8 9 10 11;6 7 9 10 15; 4 6 12 15 16];
 
-% load '..\RFData\rf01.mat'
-% OriginalImg = RfDataDouble(1:1700,:);
-% maxIm = max(OriginalImg(:));
-% OriginalImg = OriginalImg/maxIm;
-% 
-% 
-% load '..\RFData\rf03.mat'
-% CompressedImg = RfDataDouble(1:1700,:);
-% CompressedImg = CompressedImg/maxIm;
+load '..\RFData\rf01.mat'
+OriginalImg = RfDataDouble(1:1700,:);
+maxIm = max(OriginalImg(:));
+OriginalImg = OriginalImg/maxIm;
+
+
+load '..\RFData\rf03.mat'
+CompressedImg = RfDataDouble(1:1700,:);
+CompressedImg = CompressedImg/maxIm;
 
 
 % Defining Parameters
 w=.15; %this is the regularization weight
 [imHight, imWidth] = size(OriginalImg);
-da_min = -2;
+da_min = -100;
 da_max = 0;
 dl_min = -2;
 dl_max = 2;
@@ -31,8 +32,9 @@ noLDisp = size(dl,2); % number of possible lateral displacements
 axialDisplacementMap = zeros(imHight, imWidth);
 lateralDisplacementMap = zeros(imHight, imWidth);
 numNegativeAxialDis = sum(da <= 0);
+numPositiveAxialDis = noADisp - numNegativeAxialDis;
 numNegativeLateralDis = sum(dl <= 0);
-
+numPositiveLateralDis = noLDisp - numNegativeLateralDis;
 
 disp 'Initialization...'
 
@@ -49,38 +51,45 @@ for colIdx = 1:imWidth
     g_prime = CompressedImg;
     stratIndexDeltaAx = numNegativeAxialDis;
     stratIndexDeltaLat = max(numNegativeLateralDis - colIdx+1,1);
-    C(1, stratIndexDeltaAx:end, stratIndexDeltaLat:end) = delta2d(1,colIdx, da(stratIndexDeltaAx:end), dl(stratIndexDeltaLat:end), g, g_prime);
+    endIndexDeltaLat = min(imWidth -colIdx, numPositiveLateralDis) + numNegativeLateralDis;
+    C(1, stratIndexDeltaAx:end, stratIndexDeltaLat:endIndexDeltaLat) = delta2d(1,colIdx, da(stratIndexDeltaAx:end), dl(stratIndexDeltaLat:endIndexDeltaLat), g, g_prime);
     M = NaN(imHight, noADisp, noLDisp,2); % 1 for Axial and 2 for Lateral
-
     for rowIdx = 2:imHight
         minimizedValues = NaN(noADisp, noLDisp);
         minimizedValuesIndex =  NaN(noADisp, noLDisp,2); % 1 for Axial and 2 for Lateral
         delta = NaN(noADisp, noLDisp);
 
         startADis = max(numNegativeAxialDis - rowIdx + 1, 1);
-        endADis = noADisp;
+        endADis = min(imHight -rowIdx, numPositiveAxialDis) + numNegativeAxialDis;
 
         for axDisIdx = startADis:endADis
             startLDis = max(numNegativeLateralDis - colIdx + 1, 1);
-            endLDis = noLDisp;
+            endLDis = min(imWidth -colIdx, numPositiveLateralDis) + numNegativeLateralDis;
             for latDisIdx = startLDis:endLDis
                 selectedAIdx = max(axDisIdx - windowSize,1):min(axDisIdx + windowSize,noADisp);
                 selectedLIdx = max(latDisIdx - windowSize,1):min(latDisIdx + windowSize,noLDisp);
               
                 P1 = reshape(Pre_C(rowIdx,selectedAIdx, selectedLIdx), length(selectedAIdx), length(selectedLIdx));
                 P2 = reshape(C(rowIdx -1 ,selectedAIdx, selectedLIdx), length(selectedAIdx), length(selectedLIdx));
+                
 
                 Smooth = S2d(da(axDisIdx), dl(latDisIdx), ...
                     repmat(da(selectedAIdx)', 1, length(selectedLIdx)), ...
                     repmat(dl(selectedLIdx),length(selectedAIdx),1));
                 
                 a = (P1 + P2)/2 + w * Smooth;
-
-               
-
+                
                 minValue = min(a(:));
-                [minAIdx,minLIdx] = find(a==minValue);
+                [minAIdx,minLIdx] = find(a==minValue, 1, 'first');
 
+                if axDisIdx == 1
+                    minAIdx = minAIdx +1;
+                end
+
+                if latDisIdx == 1
+                    minLIdx = minLIdx+1;
+                end
+                
                 expectedAIdx = (minAIdx - 2) + axDisIdx;
                 expectedAIdx = max(expectedAIdx,1);
                 expectedLIdx = (minLIdx - 2) + latDisIdx;
@@ -96,10 +105,12 @@ for colIdx = 1:imWidth
         M(rowIdx, :, :, :) = minimizedValuesIndex;
     end
 
+
     lastCost = reshape(C(imHight,:,:), noADisp, noLDisp);
     
     minCost = min(lastCost(:));
-    [minACIdx,minLCIdx] = find(lastCost==minCost);
+    [minACIdx,minLCIdx] = find(lastCost==minCost, 1, 'first');
+    
     D_idx = zeros(imHight,2); % 1 for Axial and 2 for Lateral
     D_idx(imHight,:) = [minACIdx, minLCIdx];
     
@@ -109,10 +120,9 @@ for colIdx = 1:imWidth
     axialDisplacementMap(:,colIdx) = da(D_idx(:,1))';
     lateralDisplacementMap(:,colIdx) = dl(D_idx(:,2))';
     Pre_C = C;
-    C = NaN(imHight,noADisp, noLDisp);  
+    C = NaN(imHight,noADisp, noLDisp); 
     toc;
 end
-
 
 disp 'Process Finished...';
 disp 'Showing Results...';
--- SourceCode/Strain.m
+++ SourceCode/Strain.m
@@ -1,21 +1,9 @@
 function strain_magnitude = Strain(img)
+    sigma = 2;
     d_x = [-1 0 1];
-    d_y = d_x';
-    strain_magnitude = conv2(img, d_x);
-    strain_magnitude = conv2(strain_magnitude, d_y);
-%     % Calculate partial derivatives in x and y directions
-%     du_dx = diff(img, 1, 2);
-%     du_dy = diff(img, 1, 1);
-%     
-%     % Pad the derivatives to match the size of the displacement map
-%     du_dx = [du_dx, zeros(size(du_dx, 1), 1)];
-%     du_dy = [du_dy; zeros(1, size(du_dy, 2))];
-%     
-%     % Calculate strain components
-%     epsilon_xx = du_dx;
-%     epsilon_yy = du_dy;
-%     epsilon_xy = 0.5 * (du_dx + du_dy);
-%     
-%     % Calculate the magnitude of the strain
-%     strain_magnitude = sqrt(epsilon_xx.^2 + epsilon_yy.^2 + 2*epsilon_xy.^2);
+
+    % Apply Gaussian filter using imgaussfilt
+    smoothed_image = imgaussfilt(img, sigma);
+
+    strain_magnitude = conv2(smoothed_image, d_x);
 end
